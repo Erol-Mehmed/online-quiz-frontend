12b4fb014a16e67fd6120d1ccc75f0185a57dcea
Erol Mehmed <erol.mehmed07@gmail.com>
2026-03-08 21:51:33 +0200

    add Tailwind CSS configuration and layout components; create Header and Container components

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,24 @@
+import Container from "./Container"
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-green border-b" >
+      <Container>
+        <div className="flex h-16 items-center justify-between">
+          <Link href="/" className="text-xl font-bold">
+            OnlineQuiz
+          </Link>
+
+          <nav className="flex gap-6">
+            <Link href="/quizzes">Quizzes</Link>
+            <Link href="/leaderboard">Leaderboard</Link>
+            <Link href="/login">Login</Link>
+            <Link href="/logout">Logout</Link>
+            <Link href="/admin">Admin</Link>
+          </nav>
+        </div>
+      </Container>
+    </header>
+  );
+}
