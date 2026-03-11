fd5e36c35973006b02ed7c8c8734430d3660255c
Erol Mehmed <erol.mehmed07@gmail.com>
2026-03-12 00:46:47 +0200

    add NavLink component to Header for active link styling; add Prettier configuration

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,26 @@
+"use client";
+
 import Container from "./Container"
 import Link from "next/link";
+import React from "react";
+import {usePathname} from "next/navigation";
+
+interface NavLinkProps {
+  children: React.ReactNode;
+  urlEndpoint: string;
+}
+
+const NavLink = ({children, urlEndpoint}: NavLinkProps) => {
+  const pathname = usePathname();
+  const isActive = pathname == urlEndpoint;
+
+  return (
+    <Link href={urlEndpoint} className={`px-4 py-2 rounded transition 
+    ${isActive ? 'text-blue-600 font-semibold' : 'text-gray-700 hover:text-blue-600 hover:bg-gray-200' }`}>
+      {children}
+    </Link>
+  );
+}
 
 export default function Header() {
   return (
@@ -11,11 +32,11 @@ export default function Header() {
           </Link>
 
           <nav className="flex gap-6">
-            <Link href="/quizzes">Quizzes</Link>
-            <Link href="/leaderboard">Leaderboard</Link>
-            <Link href="/login">Login</Link>
-            <Link href="/logout">Logout</Link>
-            <Link href="/admin">Admin</Link>
+            <NavLink urlEndpoint="/quizzes">Quizzes</NavLink>
+            <NavLink urlEndpoint="/leaderboard">Leaderboard</NavLink>
+            <NavLink urlEndpoint="/login">Login</NavLink>
+            <NavLink urlEndpoint="/logout">Logout</NavLink>
+            <NavLink urlEndpoint="/admin">Admin</NavLink>
           </nav>
         </div>
       </Container>
